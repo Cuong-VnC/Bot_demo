976cc567da8ce8c8c640b511db3b4142845dfdf6
Vũ Nhật Cường <95034506+Cuong-VnC@users.noreply.github.com>
2026-06-07 15:49:57 +0700

    Update be/backend/uploaders/__init__.py via auto deploy script

---
+++ be/backend/uploaders/__init__.py
@@ -0,0 +1,9 @@
+from .youtube import upload_to_youtube
+from .facebook import upload_to_facebook
+from .tiktok import upload_to_tiktok
+
+__all__ = [
+    'upload_to_youtube',
+    'upload_to_facebook',
+    'upload_to_tiktok'
+]
